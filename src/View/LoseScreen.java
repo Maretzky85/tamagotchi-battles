f32b1c3832773b4e96d66cb9584c66c66a8e7979
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-12-10 13:55:15 +0100

    loser jpg added

---
+++ src/View/LoseScreen.java
@@ -0,0 +1,29 @@
+package View;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class LoseScreen {
+
+    public GridPane lose(){
+        GridPane gridPane = new GridPane();
+        Button button = new Button("Restart Game");
+
+
+        button.setOnAction(e -> System.out.println("but"));
+
+
+        Image image = new Image("/resources/loser.jpg");
+        ImageView imageView = new ImageView(image);
+        gridPane.add(button, 1, 100);
+        gridPane.add(imageView, 1, 1);
+        gridPane.setAlignment(Pos.CENTER);
+
+        return gridPane;
+    }
+}
